1e8b473835f3daae7040464cf1be0672d4405a27
Michal Balazi <balazi.michal@gmail.com>
2018-09-06 16:46:32 +0200

    feat - adding gulpfile
    Adding basic settings for gulpfile.js

---
+++ gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp');
+
+// styles
+gulp.task('default', function() {
+  console.log('sass processing');
+});
+
+// scripts
+gulp.task('default', function() {
+  console.log('script minifying');
+});
+
+// images
+gulp.task('default', function() {
+  console.log('images compressing');
+});
+
+// watch
+gulp.task('default', function() {
+  console.log('watching...');
+});
+
+// build
+gulp.task('default', function() {
+  console.log('building...');
+});
+
+// default task
+gulp.task('default', function() {
+  console.log('default task running...');
+});
