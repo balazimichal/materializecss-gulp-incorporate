324f4949f97e97cd12b61caa2bc2f894ba784d99
Michal Balazi <balazi.michal@gmail.com>
2018-09-08 08:18:47 +0200

    feat - header section
    Added header section and foundation of all the other sections. Scrollspy now working.

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -1,1 +1,9 @@
-console.log('working');
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.sidenav');
+  var instances = M.Sidenav.init(elems);
+});
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.scrollspy');
+  var instances = M.ScrollSpy.init(elems);
+});
