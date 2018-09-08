3810bcb9b3c0fedd05aeb649c3259cf8a4eee799
Michal Balazi <balazi.michal@gmail.com>
2018-09-08 08:50:15 +0200

    feat - navigation pinned
    Pinned the navigation making it initially transparent and dark grey-blue once scrolled.

--- dev/js/scripts.js
+++ dev/js/scripts.js
@@ -7,3 +7,19 @@ document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.scrollspy');
   var instances = M.ScrollSpy.init(elems);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+  var elems = document.querySelectorAll('.pushpin');
+  var options = {
+    top: 10,
+    onPositionChange: function() {
+      var element = document.getElementById('main-nav');
+      if (document.querySelector('.pinned') !== null) {
+        element.classList.add('pinned');
+      } else {
+        element.classList.remove('pinned');
+      }
+    }
+  };
+  var instances = M.Pushpin.init(elems, options);
+});
